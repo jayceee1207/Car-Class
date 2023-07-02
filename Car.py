1139bd6cbaa451161866824e3e3a3c77eb049170
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 00:08:46 +0800

    model, brand, and speed of car

--- Car.py
+++ Car.py
@@ -1,7 +1,10 @@
 #create a class car
-
+class Car:
     #def init (model, brand, speed)
-
+    def __init__ (self, model=2022, make="Chevrolet Trailblazer", speed=0):
+        self.__year_model = model
+        self.__make = make
+        self.__speed = speed
     #accelerate
 
     #break

f31faefc3aa9840fbb45cbc804f132e7cd3ec965
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 00:17:12 +0800

    pseudocode in main file, call the objects

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 #BSCPE 1-5
 #Car Class
 #July 1, 2023
+
+#import car
+
+#create an object
+
+#Call the accelerate method five times.
+#Get the current speed of the car after each call and display it.
+
+
+#Calls the brake method five times.
+#Get the current speed of the car after each call and display it.

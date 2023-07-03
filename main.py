195fd363893870028c5e369fbb072ac572f6f01e
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 08:48:24 +0800

    import car file and create a object

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 #July 1, 2023
 
 #import car
-
+from Car import Car
 #create an object
+car1 = Car()
 
 #Call the accelerate method five times.
 #Get the current speed of the car after each call and display it.

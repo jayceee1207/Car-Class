655bfbd8246a566b1bad834ee3e259811d085e00
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 08:59:46 +0800

    Car 1 get the info, speed, accelerate

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ from Car import Car
 car1 = Car()
 
 #Call the accelerate method five times.
+print("**ACCELERATE -- 5 times**")
+car1.accelerate()
+car1.get_speed()
+print("[1]")
+car1.showInfo()
+
+car1.accelerate()
+car1.get_speed()
+print("[2]")
+car1.showInfo()
+
+car1.accelerate()
+car1.get_speed()
+print("[3]")
+car1.showInfo()
+
+car1.accelerate()
+car1.get_speed()
+print("[4]")
+car1.showInfo()
+
+car1.accelerate()
+car1.get_speed()
+print("[5]")
+car1.showInfo()
+
 #Get the current speed of the car after each call and display it.
 
 
